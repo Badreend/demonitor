58407a39d2a25c6f2dea105234a4c4d5f9db70e6
Ismaili <bismai01@KN200993.local>
2017-02-13 15:05:06 +0100

    save arezzo files

--- index.js
+++ index.js
@@ -107,9 +107,7 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-});
-
+http.listen(process.env.PORT || 3000);
 
 function sendMsgData() {
 	io.emit('newConnection',msgData);	
